88c12160b11c2389d96c917a8a9e417eeff67066
Johan <johanmunneke94@gmail.com>
2019-10-17 15:42:55 +0200

    Setup widget

--- src/FeedbackNotification.jsx
+++ src/FeedbackNotification.jsx
@@ -1,12 +1,65 @@
 import { Component, createElement } from "react";
 import { hot } from "react-hot-loader/root";
 
-import { HelloWorldSample } from "./components/HelloWorldSample";
+import { Notification } from "./components/Notification";
 import "./ui/FeedbackNotification.css";
 
 class FeedbackNotification extends Component {
+    constructor(props) {
+        super(props);
+        var position = '';
+    }
+    componentDidMount() {
+        //Set position of the toast message
+        var tempPosition = this.props.position;
+        if (tempPosition === 'topLeft') {
+            this.position = 'top-left';
+        } else if (tempPosition === 'topRight') {
+            this.position = 'top-right';
+        } else if (tempPosition === 'topCenter') {
+            this.position = 'top-center';
+        } else if (tempPosition === 'bottomRight') {
+            this.position = 'bottom-right';
+        } else if (tempPosition === 'bottomLeft') {
+            this.position = 'bottom-left';
+        } else {
+            this.position = 'bottom-center';
+        }
+    }
     render() {
-        return <HelloWorldSample sampleText={this.props.sampleText} />;
+        var autoClose = 5000;
+        // If autoClose is set on a specific notification, use this value, else use value set on container
+        if (typeof this.props.autoCloseNotification === 'undefined') {
+            autoClose = this.props.autoCloseContainer;
+        } else {
+            autoClose = parseInt(this.props.autoCloseNotification.displayValue, 0) 
+        }
+        // If autoclose is 0, set to false, then user has to close message himself
+        if (autoClose === 0) {
+            autoClose = false;
+        }
+
+        // get className and notificationText, can both be empty
+        var notificationText = typeof this.props.notificationText === 'undefined' ? '' : this.props.notificationText.value;
+        var className = typeof this.props.className === 'undefined' ? undefined : this.props.notificationText.value;
+
+        return <Notification 
+            showNotification = {this.props.showNotification}
+            position = {this.position}
+            autoClose = {autoClose}
+            closeButton = {this.props.closeButton === false ? false : undefined}
+            closeOnClick = {this.props.closeOnClick}
+            draggable = {this.props.draggable}
+            draggablePercent = {this.props.draggablePercent}
+            notificationText = {notificationText}
+            className = {className}
+            transition = {this.props.transition}
+            hideProgressBar = {this.props.hideProgressBar}
+            pauseOnHover = {this.props.pauseOnHover}
+            pauseOnFocusLoss = {this.props.pauseOnFocusLoss}
+            rtl = {this.props.rtl}
+            newestOnTop = {this.props.newestOnTop}
+        />;
     }
 }
 
---
+++ src/components/Notification.jsx
@@ -0,0 +1,45 @@
+import { Component, createElement } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Slide, Zoom, Flip, Bounce } from 'react-toastify';
+
+export class Notification extends Component {
+    componentDidUpdate () {
+        // check if need to show notification
+        if(this.props.showNotification.value) {
+            toast(this.props.notificationText, {
+                autoClose: this.props.autoClose,
+                className: this.props.className
+            });
+            this.props.showNotification.setValue(false);
+        }
+    }
+
+    render() {
+        var transition;
+        if (this.props.transition === 'Slide') {
+            transition = Slide;
+        } else if (this.props.transition === 'Zoom') {
+            transition = Zoom;
+        } else if (this.props.transition === 'Flip') {
+            transition = Flip;
+        } else {
+            transition = Bounce;
+        }
+
+        return <ToastContainer 
+                position = {this.props.position}
+                closeButton = {this.props.closeButton}
+                closeOnClick = {this.props.closeOnClick}
+                draggable = {this.props.draggable}
+                draggablePercent = {this.props.draggablePercent}
+                transition = {transition}
+                hideProgressBar = {this.props.hideProgressBar}
+                pauseOnHover = {this.props.pauseOnHover}
+                pauseOnFocusLoss = {this.props.pauseOnFocusLoss}
+                rtl = {this.props.rtl}
+                newestOnTop = {this.props.newestOnTop}
+            />;
+    }
+}
+
