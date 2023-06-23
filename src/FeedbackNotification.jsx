91a10302abd4a44b002d647bc0a5d62b1d241794
Johan Munneke <johanmunneke94@gmail.com>
2023-06-23 15:11:56 +0200

    updated dependencies and to functional component

--- src/FeedbackNotification.jsx
+++ src/FeedbackNotification.jsx
@@ -1,83 +1,73 @@
-import { Component, createElement } from "react";
+import { createElement } from "react";
 
-import { Notification } from "./components/Notification";
 import "./ui/FeedbackNotification.css";
+import { Notification } from "./components/Notification";
 
-export default class FeedbackNotification extends Component {
-    constructor(props) {
-        super(props);
-        var position = '';
-        this.onShowAction = this.executeShowAction.bind(this);
-        this.onCloseAction = this.executeCloseAction.bind(this);
-    }
-    componentDidMount() {
-        //Set position of the toast message
-        var tempPosition = this.props.position;
-        if (tempPosition === 'topLeft') {
-            this.position = 'top-left';
-        } else if (tempPosition === 'topRight') {
-            this.position = 'top-right';
-        } else if (tempPosition === 'topCenter') {
-            this.position = 'top-center';
-        } else if (tempPosition === 'bottomRight') {
-            this.position = 'bottom-right';
-        } else if (tempPosition === 'bottomLeft') {
-            this.position = 'bottom-left';
-        } else {
-            this.position = 'bottom-center';
-        }
-    }
+export default function FeedbackNotification(props) {
 
-    executeShowAction() {
-        this.props.showNotification.setValue(false);
+    const executeShowAction = () => {
+        props.showNotification.setValue(false);
         //Execute the on show action if needed
-        if (this.props.onShowAction && this.props.onShowAction.canExecute) {
-            this.props.onShowAction.execute();
+        if (props.onShowAction && props.onShowAction.canExecute) {
+            props.onShowAction.execute();
         }
     }
 
-    executeCloseAction() {
+    const executeCloseAction = () => {
         //Execute the on close action if needed
-        if (this.props.onCloseAction && this.props.onCloseAction.canExecute) {
-            this.props.onCloseAction.execute();
+        if (props.onCloseAction && props.onCloseAction.canExecute) {
+            props.onCloseAction.execute();
         }
     }
 
-    render() {
-        var autoClose = 5000;
-        // If autoClose is set on a specific notification, use this value, else use value set on container
-        if (typeof this.props.autoCloseNotification === 'undefined') {
-            autoClose = this.props.autoCloseContainer;
-        } else {
-            autoClose = parseInt(this.props.autoCloseNotification.displayValue, 0) 
-        }
-        // If autoclose is 0, set to false, then user has to close message himself
-        if (autoClose === 0) {
-            autoClose = false;
-        }
 
-        // get className and notificationText, can both be empty
-        var notificationText = typeof this.props.notificationText === 'undefined' ? '' : this.props.notificationText.value;
-        var className = typeof this.props.className === 'undefined' ? undefined : this.props.className.value;
-
-        return <Notification 
-            showNotification = {this.props.showNotification}
-            position = {this.position}
-            autoClose = {autoClose}
-            closeButton = {this.props.closeButton === false ? false : undefined}
-            closeOnClick = {this.props.closeOnClick}
-            draggable = {this.props.draggable}
-            draggablePercent = {this.props.draggablePercent}
-            notificationText = {notificationText}
-            className = {className}
-            transition = {this.props.transition}
-            hideProgressBar = {this.props.hideProgressBar}
-            pauseOnHover = {this.props.pauseOnHover}
-            pauseOnFocusLoss = {this.props.pauseOnFocusLoss}
-            rtl = {this.props.rtl}
-            newestOnTop = {this.props.newestOnTop}
-            onShowAction = {this.onShowAction}
-            onCloseAction = {this.onCloseAction}
-        />;
+    // Convert position
+    let position = '';
+    if (props.position === 'topLeft') {
+        position = 'top-left';
+    } else if (props.position === 'topRight') {
+        position = 'top-right';
+    } else if (props.position === 'topCenter') {
+        position = 'top-center';
+    } else if (props.position === 'bottomRight') {
+        position = 'bottom-right';
+    } else if (props.position === 'bottomLeft') {
+        position = 'bottom-left';
+    } else {
+        position = 'bottom-center';
     }
+
+    let autoClose = props.autoCloseContainer;
+    // If autoClose is set on a specific notification, use this value, else use value set on container
+    if (props.autoCloseNotification !== undefined) {
+        autoClose = parseInt(props.autoCloseNotification.displayValue, 0);
+    }
+    // If autoclose is 0, set to false, then user has to close message himself
+    if (autoClose === 0) {
+        autoClose = false;
+    }
+
+    // get className and notificationText, can both be empty
+    let notificationText = props.notificationText === undefined ? '' : props.notificationText.value;
+    let className = props.className === undefined ? undefined : props.className.value;
+
+    return <Notification 
+        showNotification = {props.showNotification}
+        position = {position}
+        autoClose = {autoClose}
+        closeButton = {props.closeButton === false ? false : undefined}
+        closeOnClick = {props.closeOnClick}
+        draggable = {props.draggable}
+        draggablePercent = {props.draggablePercent}
+        notificationText = {notificationText}
+        className = {className}
+        transition = {props.transition}
+        hideProgressBar = {props.hideProgressBar}
+        pauseOnHover = {props.pauseOnHover}
+        pauseOnFocusLoss = {props.pauseOnFocusLoss}
+        rtl = {props.rtl}
+        newestOnTop = {props.newestOnTop}
+        onShowAction = {executeShowAction}
+        onCloseAction = {executeCloseAction}
+    />;
 }
--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,55 +1,46 @@
-import { Component, createElement } from "react";
+import { createElement, useEffect } from "react";
+
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Slide, Zoom, Flip, Bounce } from 'react-toastify';
 
-export class Notification extends Component {
-    constructor(props) {
-        super(props);
-        this.onCloseAction = this.onClose.bind(this);
-    }
-    componentDidUpdate () {
-        // check if need to show notification
-        if(this.props.showNotification.value) {
-            toast(this.props.notificationText, {
-                autoClose: this.props.autoClose,
-                className: this.props.className,
-                onClose: this.onCloseAction
+export function Notification(props) {
+
+    useEffect(() => {
+        if (props.showNotification.value) {
+            toast(props.notificationText, {
+                autoClose: props.autoClose,
+                className: props.className,
+                onClose: props.onCloseAction
             });
-            this.props.onShowAction();
+            props.onShowAction();
         }
+    }, [props.showNotification.value])
+
+    // Convert transition prop to actual transition
+    let transition;
+    if (props.transition === 'Slide') {
+        transition = Slide;
+    } else if (props.transition === 'Zoom') {
+        transition = Zoom;
+    } else if (props.transition === 'Flip') {
+        transition = Flip;
+    } else {
+        transition = Bounce;
     }
 
-    onClose () {
-        this.props.onCloseAction();
-    }
-
-    render() {
-        // Convert transition prop to actual transition
-        var transition;
-        if (this.props.transition === 'Slide') {
-            transition = Slide;
-        } else if (this.props.transition === 'Zoom') {
-            transition = Zoom;
-        } else if (this.props.transition === 'Flip') {
-            transition = Flip;
-        } else {
-            transition = Bounce;
-        }
-
-        return <ToastContainer 
-                position = {this.props.position}
-                closeButton = {this.props.closeButton}
-                closeOnClick = {this.props.closeOnClick}
-                draggable = {this.props.draggable}
-                draggablePercent = {this.props.draggablePercent}
-                transition = {transition}
-                hideProgressBar = {this.props.hideProgressBar}
-                pauseOnHover = {this.props.pauseOnHover}
-                pauseOnFocusLoss = {this.props.pauseOnFocusLoss}
-                rtl = {this.props.rtl}
-                newestOnTop = {this.props.newestOnTop}
-            />;
-    }
+    return <ToastContainer 
+            position = {props.position}
+            closeButton = {props.closeButton}
+            closeOnClick = {props.closeOnClick}
+            draggable = {props.draggable}
+            draggablePercent = {props.draggablePercent}
+            transition = {transition}
+            hideProgressBar = {props.hideProgressBar}
+            pauseOnHover = {props.pauseOnHover}
+            pauseOnFocusLoss = {props.pauseOnFocusLoss}
+            rtl = {props.rtl}
+            newestOnTop = {props.newestOnTop}
+        />;
 }
 
